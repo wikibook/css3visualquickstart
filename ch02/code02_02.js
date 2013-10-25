84211832c524bb644571a3c7dd7eca710fde7d61
위키북스 <dylee@wikibook.co.kr>
2013-10-25 15:35:35 +0900

    <쉽고 빠르게 익히는 CSS3> 소스코드

---
+++ ch02/code02_02.js
@@ -0,0 +1,25 @@
+/*** CSS VQS - Chapter 2 - HTML5forIE.js ***/
+document.createElement('abbr');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('audio');
+document.createElement('bb');
+document.createElement('canvas');
+document.createElement('datagrid');
+document.createElement('datalist');
+document.createElement('details');
+document.createElement('dialog');
+document.createElement('eventsource');
+document.createElement('figure');
+document.createElement('footer');
+document.createElement('header');
+document.createElement('hgroup');
+document.createElement('mark');
+document.createElement('menu');
+document.createElement('meter');
+document.createElement('nav');
+document.createElement('output');
+document.createElement('progress');
+document.createElement('section');
+document.createElement('time');
+document.createElement('video');
